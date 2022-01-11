cbeb09dfebae6f2795c13a3ee0b1263786441a87
nderimmM <nderim.murati321@gmail.com>
2022-01-11 12:20:19 +0100

    kirjimi i data.js dhe ndryshimi ti te dhenave nga statike ne dinamike

---
+++ frontend/src/data.js
@@ -0,0 +1,79 @@
+const data = {
+
+    products: [
+        {
+            _id:'1',
+            name:'Samsung Galaxy S22 Ultra',
+            category:'Phones',
+            image:'/images/p1.jpg',
+            price:2900,
+            brand:'Samsung',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Phone by Samsung',
+        },
+
+        {
+            _id:'2',
+            name:'Iphone 13 Pro Max',
+            category:'Phones',
+            image:'/images/p2.jpg',
+            price:2800,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Phone by Apple',
+        },
+
+        {
+            _id:'3',
+            name:'Xiaomi Remdi Note11 Pro',
+            category:'Phones',
+            image:'/images/p3.jpg',
+            price:950,
+            brand:'Xiaomi',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Phone by Xiaomi',
+        },
+
+        {
+            _id:'4',
+            name:'MacBook Pro',
+            category:'Laptops',
+            image:'/images/p4.png',
+            price:1200,
+            brand:'Apple',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Laptop by Apple ',
+        },
+
+        {
+            _id:'5',
+            name:'Nvidia RTX 3090',
+            category:'Graphic Cards',
+            image:'/images/p5.png',
+            price:2780,
+            brand:'Nvidia',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Graphic Card by Nvidia',
+        },
+
+        {
+            _id:'6',
+            name:'Nvidia Gtx 970',
+            category:'Graphic Cards',
+            image:'/images/p6.jpg',
+            price:350,
+            brand:'Nvidia',
+            rating:4.5,
+            numReviews:10,
+            description:'Amazing Graphic Card by Nvidia',
+        },
+    ],
+
+}
+
+export default data;
